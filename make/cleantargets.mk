87e615d52c85157fba3056e80b891390db190d83
mohousch <mohousch@gmail.com>
2025-09-16 20:15:09 +0200

    - Cleanup.

--- make/cleantargets.mk
+++ make/cleantargets.mk
@@ -34,7 +34,7 @@ distclean:
 #
 driver-clean:
 ifeq ($(BOXARCH), sh4)
-	$(MAKE) -C $(DRIVER_DIR) ARCH=sh KERNEL_LOCATION=$(KERNEL_DIR) distclean
+	$(MAKE) -C $(DRIVER_DIR) ARCH=sh KERNEL_LOCATION=$(KERNEL_DIR) clean
 else
 	rm -f $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/extra/*
 endif
