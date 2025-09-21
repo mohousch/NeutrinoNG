26c6365d81c5fac064f42db708ae68c91f2f61ae
mohousch <mohousch@gmail.com>
2025-09-21 06:45:04 +0200

    - Update Build.

--- make/buildenv.mk
+++ make/buildenv.mk
@@ -92,6 +92,21 @@ TARGET		     ?= mipsel-unknown-linux-gnu
 TARGET_MARCH_CFLAGS   = -march=mips32 -mtune=mips32
 endif
 
+ifeq ($(BOXARCH), powerpc)
+TARGET		     ?= powerpc-unknown-linux-gnu
+TARGET_MARCH_CFLAGS   =
+endif
+
+ifeq ($(BOXARCH), aarch64)
+TARGET		     ?= aarch64-unknown-linux-gnu
+TARGET_MARCH_CFLAGS   =
+endif
+
+ifeq ($(BOXARCH), x86_64)
+TARGET		     ?= x86_64-unknown-linux-gnu
+TARGET_MARCH_CFLAGS   =
+endif
+
 OPTIMIZATIONS        ?= size
 ifeq ($(OPTIMIZATIONS), size)
 TARGET_O_CFLAGS       = -Os
