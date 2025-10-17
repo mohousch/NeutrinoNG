8c9016dda0147f799082fa7d48d83bb4987c599f
mohousch <mohousch@gmail.com>
2025-10-17 16:12:21 +0200

    - [hd51] try to fix images.

--- helpers/gfuture/gfuture.mk
+++ helpers/gfuture/gfuture.mk
@@ -123,11 +123,7 @@ gfuture-rootfs-image-$(BOXTYPE):
 	rm -rf $(IMAGE_BUILD_DIR) || true
 	mkdir -p $(IMAGE_BUILD_DIR)/$(FLASHIMAGE_PREFIX)
 	# kernel
-ifneq ($(KERNEL_DTB_VER),)
-	cp $(TARGET_DIR)/boot/zImage.dtb $(IMAGE_BUILD_DIR)/$(FLASHIMAGE_PREFIX)/$(KERNEL_FILE)
-else
-	cp $(TARGET_DIR)/boot/zImage $(IMAGE_BUILD_DIR)/$(FLASHIMAGE_PREFIX)/$(KERNEL_FILE)
-endif
+	cp $(TARGET_DIR)/boot/zImage* $(IMAGE_BUILD_DIR)/$(FLASHIMAGE_PREFIX)/$(KERNEL_FILE)
 	# rootfs
 	cd $(RELEASE_DIR); \
 	tar -cvf $(IMAGE_BUILD_DIR)/$(FLASHIMAGE_PREFIX)/rootfs.tar --exclude=zImage* . > /dev/null 2>&1; \
