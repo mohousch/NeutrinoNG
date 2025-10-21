28f2388336064b7a6c71f93788f056c608b243fa
mohousch <mohousch@gmail.com>
2025-10-21 18:20:05 +0200

    - Update.

--- root/etc/mdev/mdev-events.sh
+++ root/etc/mdev/mdev-events.sh
@@ -3,15 +3,17 @@ while [ -z "$(mount | grep '/dev ')" ];do sleep 1;done
 [ "$SHLVL" != "2" ] && exit 0
 ENABLE_LOG=1
 LOG="/dev/.mdev/mdev-events.log"
+
 #
 loginfo()
 {
-OUT=$1
-logleft="[$ACTION] $(date +'%H:%M:%S') [$MDEV]"
-if [ "$ENABLE_LOG" == "1" ];then
-	echo "$logleft $OUT" >> $LOG
-fi
+	OUT=$1
+	logleft="[$ACTION] $(date +'%H:%M:%S') [$MDEV]"
+	if [ "$ENABLE_LOG" == "1" ];then
+		echo "$logleft $OUT" >> $LOG
+	fi
 }
+
 env_log() {
 	if [ -n "$(echo $MDEV | grep $1)" ];then
 		env >> /dev/.mdev/$MDEV.log
