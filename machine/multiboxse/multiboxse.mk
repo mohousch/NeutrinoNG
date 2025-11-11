38e2393cf62b18d8d5d7ba9ffde6d04720b8d1f8
mohousch <mohousch@gmail.com>
2025-11-11 18:11:54 +0100

    - Update.

--- machine/multiboxse/multiboxse.mk
+++ machine/multiboxse/multiboxse.mk
@@ -6,7 +6,7 @@
 #
 
 BOXARCH = arm
-MACHINE_OPTS = --enable-ci
+MACHINE_OPTS = --enable-ci--enable-fkeys
 
 #
 # kernel
--- machine/sf8008m/sf8008m.mk
+++ machine/sf8008m/sf8008m.mk
@@ -6,7 +6,7 @@
 #
 
 BOXARCH = arm
-MACHINE_OPTS = --enable-ci --enable-4digits
+MACHINE_OPTS = --enable-ci --enable-4digits --enable-fkeys
 
 #
 # kernel
