e49734e162ce224b1b90087116a845ec2ec1a1a3
mohousch <mohousch@gmail.com>
2026-09-14 19:07:19 +0200

    - Cleanup.

--- helpers/allwinner/post-build.sh
+++ helpers/allwinner/post-build.sh
@@ -5,10 +5,5 @@ HOST_DIR=$2
 
 PARTUUID="$($HOST_DIR/bin/uuidgen)"
 
-#sed -e "default" \
-#	-e "s/%LINUXIMAGE%/zImage/g" \
-#	-e "s/%PARTUUID%/$PARTUUID/g" \
-#	"$BINARIES_DIR/extlinux.conf"
-sed -i "s/%LINUXIMAGE%/zImage/g" "$BINARIES_DIR/extlinux.conf"
 sed -i "s/%PARTUUID%/$PARTUUID/g" "$BINARIES_DIR/extlinux.conf"
 sed -i "s/%PARTUUID%/$PARTUUID/g" "$BINARIES_DIR/genimage.cfg"
